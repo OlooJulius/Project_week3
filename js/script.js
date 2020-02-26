77891008adcc76c8e78ce5eb864997f969201478
Oloo Julius <juliusolooolero@gmail.com>
2020-02-26 16:38:46 +0300

    add hover effects using jquery onto the portfolio container

--- js/script.js
+++ js/script.js
@@ -21,3 +21,97 @@ $(document).ready(function(){
 function myFunction() {
   alert("We have received your message. Thank you for contacting us.");
 }
+
+//portfolio 
+// initialize the function
+$(document).ready(function(){
+  
+  // to show p when mouse hovers above the image
+  $("#jim").mouseover(function(){
+    $("#sudo").show();
+
+ // to show p when mouse hovers from the image
+  }).mouseout(function(){
+    $("#sudo").hide();
+  });
+
+});
+
+
+$(document).ready(function(){
+
+  $("#sar").mouseover(function(){
+    $("#sudo2").show();
+
+  }).mouseout(function(){
+    $("#sudo2").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#john").mouseover(function(){
+    $("#sudo3").show();
+
+  }).mouseout(function(){
+    $("#sudo3").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#doe").mouseover(function(){
+    $("#sudo4").show();
+
+  }).mouseout(function(){
+    $("#sudo4").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#mark").mouseover(function(){
+    $("#sudo5").show();
+
+  }).mouseout(function(){
+    $("#sudo5").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#sol").mouseover(function(){
+    $("#sudo6").show();
+
+  }).mouseout(function(){
+    $("#sudo6").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#soul").mouseover(function(){
+    $("#sudo7").show();
+
+  }).mouseout(function(){
+    $("#sudo7").hide();
+  });
+});
+
+
+$(document).ready(function(){
+
+  $("#gen").mouseover(function(){
+    $("#sudo8").show();
+
+  }).mouseout(function(){
+    $("#sudo8").hide();
+  });
+});
+
+
